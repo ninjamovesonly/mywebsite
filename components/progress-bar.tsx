1e07b83d05061b57e8b9a83e9a09c713cb6f1788
Chinedu Abalogu <chineduabalogu@gmail.com>
2022-03-13 12:55:32 +0100

    fix progress bar

--- components/progress-bar.tsx
+++ components/progress-bar.tsx
@@ -9,12 +9,9 @@ export default function ProgressBar({
 }) {
   const calculatePercentage = () => {
     let now = new Date(Date.now()).getTime();
-    console.log("times", endTime, startTime);
     let fullDistance = endTime - startTime;
     let currentDistance = now - startTime;
-    let diff = fullDistance - currentDistance;
     let per = Math.round((currentDistance / fullDistance) * 100);
-    console.log("dist", now, fullDistance, currentDistance, per, diff);
     return per;
   };
 
